467df39c463296fef228558e8f21181f996bf277
Shashank <s_shashank@outlook.com>
2018-08-25 02:11:15 +0530

    display album art and button component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = (props) => {
   const {title, artist, url, image, artist_img} = props.albumDetail;
@@ -16,6 +17,14 @@ const AlbumDetail = (props) => {
           <Text>{artist}</Text>
         </View>
       </CardSection>
+
+      <CardSection>
+        <Image source={{uri: image}} style={styles.albumArtStyle} />
+      </CardSection>
+
+      <CardSection>
+        <Button />
+      </CardSection>
     </Card>
   )
 }
@@ -27,12 +36,36 @@ const styles = {
     marginLeft: 10
   },
   title: {
-    color: '#000000'
+    color: '#000000',
+    fontSize: 18
   },
   thumbnail: {
     height: 50,
     width: 50,
     borderRadius: 30
+  },
+  albumArtStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  },
+  btnStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 2,
+    borderWidth: 1,
+    borderColor: '#17a2b8',
+    marginLeft: 5,
+    marginRight: 5
+  },
+  textStyle: {
+    fontSize: 16,
+    color: '#17a2b8',
+    alignSelf: 'center',
+    fontWeight: '600',
+    paddingTop: 5,
+    paddingBottom: 5
   }
 }
 
